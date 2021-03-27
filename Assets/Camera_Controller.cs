1c2fbc249421bd2b13396e692764de451ba2b766
teoman <kerteo.22@robcol.k12.tr>
2021-03-28 02:22:58 +0300

    lesson3

---
+++ Assets/Camera_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+
+	public Playercontroller player;
+
+	public bool isFollowing;
+
+	public float xOffset;
+
+	public float yOffest;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		player = FindObjectOfType<Playercontroller>();
+
+		isFollowing = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (isFollowing)
+		{
+			transform.position = new Vector3(player.transform.position.x + xOffset, player.transform.position.y + yOffest, transform.position.z);
+		}
+
+	}
+}
